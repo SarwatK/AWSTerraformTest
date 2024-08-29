1912bdc453433746991b29aa4b5375ec55960f0e
SarwatK <sarwat.kadri@capgemini.com>
2024-08-29 21:59:29 +0530

    vpc010

--- VPCcreation/main.tf
+++ VPCcreation/main.tf
@@ -3,12 +3,10 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
+      region = "us-east-1"
     }
   }
 }
-provider"aws"{
-  region = "us-east-1"
-} 
 
 resource "aws_vpc" "my_vpc" {
   cidr_block           = "10.0.0.0/16"
