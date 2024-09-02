5532ccbdc31d49f7b1dc5739777bc2ea4d7c0817
SarwatK <sarwat.kadri@capgemini.com>
2024-09-02 20:05:25 +0530

    s3try4

--- S3bucket/S3bucket.tf
+++ S3bucket/S3bucket.tf
@@ -1,6 +1,5 @@
 resource "aws_s3_bucket" "myawsbucket0168" {
   bucket = "myawsbucket0168"  # Ensure this name is globally unique
-  acl    = "public-read-write"
  
   tags = {
     Name        = "myawsbucket0168"
