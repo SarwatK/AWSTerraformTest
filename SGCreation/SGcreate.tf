4df5cffc2e693ab1a1459ce27cb5b4d470789195
SarwatK <sarwat.kadri@capgemini.com>
2024-09-02 18:15:53 +0530

    sg03

--- SGCreation/SGcreate.tf
+++ SGCreation/SGcreate.tf
@@ -32,14 +32,3 @@ resource "aws_security_group" "example_sg" {
     Name = "example-security-group"
   }
 }
-
-# Define a VPC (if needed)
-resource "aws_vpc" "MyVPC" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
-
-  tags = {
-    Name = "Myvpc"
-  }
-}
